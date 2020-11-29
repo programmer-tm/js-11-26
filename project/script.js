3ec49a5528444df7eb91bfdddecf2e9cc07c6c8b
tep-roman <roman_teplov@inbox.ru>
2020-11-29 23:22:34 +0300

    lessonOne

--- project/script.js
+++ project/script.js
@@ -5,18 +5,16 @@ const goods = [
   { title: 'Монитор', price: 10000 },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `
-      <div class="item">
-        <h4>${title}</h4>
-        <p>${price}</p>
-      </div>
-    `;
-}
+const renderGoodsItem = (title = "Колонки", price = 3000) => `
+    <div class="item">
+    <h4>${title}</h4>
+    <p>${price}</p></div>`;
+// выше убрал return и {}, также установил значения по умолчани(правда, это не работает, рабочий вариант я так и не понял как сделать).
 
 const renderGoods = (list) => {
-  const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods').innerHTML = goodsList;
-}
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods').innerHTML = goodsList.join("");
+    //так как за счет метода .map мы каждый div class="item" получаем в виде элемента массива, а эмементы массива разделяются запятыми. Применив к массиву метод join(""), мы получам строку и избавляемся от запятой.
+};
 
 renderGoods(goods);
