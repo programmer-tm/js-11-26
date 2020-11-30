be3d393f8c4bef8f03d216f2c894214c73d84a44
Filipp Vasilev <ampa1r.pl@gmail.com>
2020-11-30 22:03:52 +0300

    lesson #2

--- project/script.js
+++ project/script.js
@@ -1,22 +1,69 @@
-const goods = [
-  { title: 'Ноутбук', price: 30000 },
-  { title: 'Клавиатура', price: 1000 },
-  { title: 'Мышь', price: 500 },
-  { title: 'Монитор', price: 10000 },
-];
+class GoodsItem {
+  constructor({ title, price }) {
+    this.title = title;
+    this.price = price;
+  }
 
-const renderGoodsItem = (title, price) => {
+  render() {
     return `
       <div class="item">
-        <h4>${title}</h4>
-        <p>${price}</p>
+        <h4>${this.title}</h4>
+        <p>${this.price}</p>
+        <button type="button">Add to basket</button>
       </div>
     `;
+  }
 }
 
-const renderGoods = (list) => {
-  const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods').innerHTML = goodsList;
+class GoodsList {
+  constructor(basket) {
+    this.goods = [];
+    this.basket = basket;
+  }
+
+  fetchData() {
+    this.goods = [
+      { title: 'Ноутбук', price: 30000 },
+      { title: 'Клавиатура', price: 1000 },
+      { title: 'Мышь', price: 500 },
+      { title: 'Монитор', price: 10000 },
+    ];
+  }
+
+  addToBasket(item) {
+    this.basket.add(item);
+  }
+
+  render() {
+    const goodsList = this.goods.map(item => {
+      const goodsItem = new GoodsItem(item);
+      return goodsItem.render();
+    });
+    document.querySelector('.goods').innerHTML = goodsList.join('');
+  }
 }
 
-renderGoods(goods);
+class Basket {
+  constructor() {
+    this.basketGoods = [];
+  }
+
+  render() {
+
+  }
+}
+
+class BasketItem {
+  constructor({ title }) {
+    this.title = title;
+  }
+
+  render() {
+    
+  }
+}
+
+const basket = new Basket();
+const goodsList = new GoodsList(basket);
+goodsList.fetchData();
+goodsList.render();
