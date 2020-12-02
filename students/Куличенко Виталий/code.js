5f50f4036aca1150a8ca496d15ee6551ae893884
unknown <begemot39@yandex.ru>
2020-12-02 23:39:18 +0300

    Подсчёт стоимости товаров

--- students/Куличенко Виталий/code.js
+++ students/Куличенко Виталий/code.js
@@ -16,6 +16,14 @@ const goods = [
   const renderGoods = (list = []) => {
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     document.querySelector('.goods').innerHTML = goodsList.join('');
+    function getAllPrice(a) {
+      let totalCount = 0;
+      for (let i = 0; i < a.length; i++) {
+        totalCount += a[i]['price'];
+      }
+      return totalCount;
+    }
+    console.log(getAllPrice(list));
   }
   
   renderGoods(goods);
