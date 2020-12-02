f6b9066e211a7ac36133982f6dbd04cf35e7daa0
Dragon <dragon.program.sib@gmail.com>
2020-12-03 00:19:18 +0700

    Updating files for the tasks of lesson 1.

--- students/Иван Иванов/Алексей Васильков/project/script.js
+++ students/Иван Иванов/Алексей Васильков/project/script.js
@@ -7,18 +7,19 @@ const goods = [
     {title: 'Монитор', price: 10000}
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `
-    <div class="item">
-        <h4>${title}</h4>
-        <p>${price}</p>
-    </div>
+const renderGoodsItem = (title = 'Название товара', price = 'Стоимость - по запросу.') =>
+    `
+        <div class="item">
+            <img class='product_img' src="" alt="">
+            <h4 class='product_name'>${title}</h4>
+            <p class='product_price'>${price}\n\u20bd</p>
+            <button type="button" class="buy_button">Купить</button>
+        </div>
     `;
-}
 
-const renderGoods = (list) => {
+const renderGoods = (list = [null]) => {
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods').innerHTML = goodsList;
-}
+    document.querySelector('.goods').innerHTML = goodsList.join('');  // .join(''); - убрали запятые после каждого товара (объединение элементов массива в строку, с возможностью в скобках указать нужный разделитель).
+};
 
 renderGoods(goods);
