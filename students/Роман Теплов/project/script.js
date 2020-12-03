f9d3a98d8a36c0ddb6f8c5ff96b6a05664db6909
tep-roman <roman_teplov@inbox.ru>
2020-12-03 16:41:15 +0300

    lesson two

--- students/Роман Теплов/project/script.js
+++ students/Роман Теплов/project/script.js
@@ -1,20 +1,86 @@
-const goods = [
-  { title: 'Ноутбук', price: 30000 },
-  { title: 'Клавиатура', price: 1000 },
-  { title: 'Мышь', price: 500 },
-  { title: 'Монитор', price: 10000 },
-];
+class GoodsItem {
+    constructor({ title, price }) {
+        this.title = title;
+        this.price = price;
+    }
 
-const renderGoodsItem = (title = "Колонки", price = 3000) => `
-    <div class="item">
-    <h4>${title}</h4>
-    <p>${price}</p></div>`;
-// выше убрал return и {}, также установил значения по умолчани(правда, это не работает, рабочий вариант я так и не понял как сделать).
+    render() {
+        return `
+      <div class="item">
+        <h4>${this.title}</h4>
+        <p>${this.price}</p>
+        <button type="button">Add to basket</button>
+      </div>
+    `;
+    }
+}
 
-const renderGoods = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods').innerHTML = goodsList.join("");
-    //так как за счет метода .map мы каждый div class="item" получаем в виде элемента массива, а эмементы массива разделяются запятыми. Применив к массиву метод join(""), мы получам строку и избавляемся от запятой.
-};
+class GoodsList {
+    constructor(basket) {
+        this.goods = [];
+        this.basket = basket;
+    }
 
-renderGoods(goods);
+    fetchData() {
+        this.goods = [
+            { title: 'Ноутбук', price: 30000 },
+            { title: 'Клавиатура', price: 1000 },
+            { title: 'Мышь', price: 500 },
+            { title: 'Монитор', price: 10000 },
+        ];
+    }
+
+    addToBasket(item) {
+        this.basket.add(item);
+    }
+
+    render() {
+        const goodsList = this.goods.map(item => {
+            const goodsItem = new GoodsItem(item);
+            return goodsItem.render();
+        });
+        document.querySelector('.goods').innerHTML = goodsList.join('');
+    }
+
+    addSum(){
+        let sumList = 0;
+        this.goods.forEach((good)=>{
+            let goodPrice = good.price;
+            sumList += goodPrice;
+        });
+        console.log(sumList);
+    }
+}
+
+class Basket {
+    constructor() {
+        this.basketGoods = [];
+    }
+
+    render() {
+
+    }
+
+    clear(){ // - то что я добавил
+
+    }
+}
+
+class BasketItem {
+    constructor({ title }) {
+        this.title = title;
+    }
+
+    render() {
+
+    }
+    clear(){ // - то что я добавил
+
+    }
+}
+
+const basket = new Basket();
+const goodsList = new GoodsList(basket);
+goodsList.fetchData();
+goodsList.render();
+goodsList.addSum();
