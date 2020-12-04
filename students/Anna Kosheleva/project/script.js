5fbbc292df90998fe2282a3533770334b32f3a59
anyakosheleva <any4kosheleva@yandex.ru>
2020-12-04 23:28:13 +0300

    dz2

--- students/Anna Kosheleva/project/script.js
+++ students/Anna Kosheleva/project/script.js
@@ -1,21 +1,130 @@
-const goods = [
-    { title: 'Ноутбук', price: 30000 },
-    { title: 'Клавиатура', price: 1000 },
-    { title: 'Мышь', price: 500 },
-    { title: 'Монитор', price: 10000 },
-  ];
-  
-  const renderGoodsItem = (title, price) => 
-      `
-        <div class="item">
-          <h4>${title}</h4>
-          <p>${price}</p>
-        </div>
-      `;
-  
-  const renderGoods = (list = []) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods').innerHTML = goodsList.join('');
+
+  class GoodsItem {
+    constructor({ title, price }) {
+        this.title = title;
+        this.price = price;
+    }
+
+    render() {
+        return`
+            <div class="item">
+                <h4>${this.title}</h4>
+                <p>${this.price}</p>
+                <button type="button">Add to basket</button>   
+            </div>
+        `; 
+    }
   }
+
+  class GoodsList {
+        constructor(basket) {
+            this.goods = [];
+            this.basket = basket;
+        }
+
+        fetchData() {
+            this.goods = [
+                { title: 'Ноутбук', price: 30000 },
+                { title: 'Клавиатура', price: 1000 },
+                { title: 'Мышь', price: 500 },
+                { title: 'Монитор', price: 10000 },  
+            ];
+        }
+
+        addToBasket(item) {
+            this.basket.add(item);
+        }
+
+        getTotalPrice() {
+            let total = 0
+            this.goods.forEach(item => {
+                total += item.price;
+            });
+            
+            console.log(total);
+            return total;
+        }
+
+        render() {
+            const goodsList = this.goods.map(item => {
+                const goodsItem = new GoodsItem(item);
+                return goodsItem.render();
+            });
+            document.querySelector('.goods').innerHTML = goodsList.join(''); 
+        }
+    } 
   
-  renderGoods(goods)
+
+    class Basket {
+        constructor() {
+            this.basketGoods = [];
+        }
+
+        addItem() {
+
+        }
+
+        removeItem() {
+
+        }
+
+        changeQuantity() {
+
+        }
+
+        clear() {
+
+        }
+        
+        fetchData() {
+
+        }
+
+        applyPromoCode() {
+
+        }
+
+        getDeliveryPrice() {
+
+        }
+
+        createOrder() {
+
+        }
+
+        getTotalPrice() {
+
+        }
+
+        render() {
+
+        }
+    }
+
+    class BasketItem {
+        constructor({ title }) {
+            this.title = title;
+        }
+
+        changeQuantity() {
+
+        }
+
+        removeItem() {
+
+        }
+
+        changeType() {
+
+        }
+
+        render() {
+
+        }
+    }
+
+    const basket = new Basket();
+    const goodsList = new GoodsList(basket);
+    goodsList.fetchData();
+    goodsList.render();
+    goodsList.getTotalPrice();
