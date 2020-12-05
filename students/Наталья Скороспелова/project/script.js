68d5003bbeae1d146198598973e40fe27680f5c9
Natalya <soltk7@yandex.ru>
2020-12-05 19:12:06 +0300

    ДЗ ко 2-му занятию

--- students/Наталья Скороспелова/project/script.js
+++ students/Наталья Скороспелова/project/script.js
@@ -1,23 +1,136 @@
-const goods = [
-  { title: 'Ноутбук', price: 30000 },
-  { title: 'Клавиатура', price: 1000 },
-  { title: 'Мышь', price: 500 },
-  { title: 'Монитор', price: 10000 },
-];
-
-const renderGoodsItem = (title, price) => {
-    return `
-      <div class="item">
-        <h4>${title}</h4>
-        <p>${price+ " \u20bd"}</p>
-        <button class ="buy">${"Купить"}</button>
-      </div>
-    `;
+class GoodsItems {
+constructor({title, price}) {
+  this.title = title;
+  this.price = price;
+}
+render() {
+  return `
+  <div class="item">
+    <h4>${this.title}</h4>
+    <p>${this.price+ " \u20bd"}</p>
+    <button class ="buy">${"Купить"}</button>
+  </div>
+`; 
+}
 }
 
-const renderGoods = (list) => {
-  const goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
-  document.querySelector('.goods').innerHTML = goodsList;
+
+class GoodList {
+constructor(){
+  this.goods = [];
+  this.basket = basket;
+}
+fetchData(){
+  this.goods = [
+    { title: 'Ноутбук', price: 30000 },
+    { title: 'Клавиатура', price: 1000 },
+    { title: 'Мышь', price: 500 },
+    { title: 'Монитор', price: 10000 },
+  ];
 }
 
-renderGoods(goods);
+addToBasket(item) {
+  this.basket.add(item);
+}
+
+getTotalPrice() {
+  const total = this.goods.reduce((acc, curVal) => acc + curVal.price, 0);
+
+  // let total=0;
+  // this.goods.forEach(item => {
+  //     total+=item.price;
+  //   });
+  console.log(total);
+  return total;
+  
+}
+
+render(){
+  const goodsList = this.goods.map(item => {
+    const goodsItem = new GoodsItems(item);
+    return goodsItem.render();
+
+  });
+  document.querySelector('.goods').innerHTML = goodsList.join('');
+
+}
+}
+
+
+class Basket {
+  constructor() {
+    this.BasketGoods = [];
+  }
+
+  
+  addItem() {
+
+  }
+
+  removeItem() {
+
+  }
+
+  changeQuantity() {
+
+  }
+
+  clear() {
+
+  }
+
+  fetchData() {
+
+  }
+
+  applyPromoCode() {
+
+  }
+
+  getDeliveryPrice() {
+
+  }
+
+  createOrder() {
+
+  }
+
+  getTotalPrice() {
+
+  }
+  render(){
+  
+  }
+  
+  }
+  
+  class BasketItem {
+    constructor({title}){
+      this.title = title;
+    }
+  
+    changeQuantity() {
+
+    }
+  
+    removeItem() {
+    }
+  
+    changeType() {
+    }
+  
+    render() {
+  
+    }
+  
+  }
+
+const basket = new Basket();
+const goodList = new GoodList(basket);
+goodList.fetchData();
+goodList.render();
+goodList.getTotalPrice();
+
+
+
+
