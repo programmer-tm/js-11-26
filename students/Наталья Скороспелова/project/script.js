a4fedd9c78ef706b4a81e1129b0518f2672b9ea4
Natalya <soltk7@yandex.ru>
2020-12-06 00:42:09 +0300

    добавлено отображение товаров через json

--- students/Наталья Скороспелова/project/script.js
+++ students/Наталья Скороспелова/project/script.js
@@ -1,3 +1,32 @@
+const API = 'https://raw.githubusercontent.com/Rikanda/js-11-26/master/students/%D0%9D%D0%B0%D1%82%D0%B0%D0%BB%D1%8C%D1%8F%20%D0%A1%D0%BA%D0%BE%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BB%D0%BE%D0%B2%D0%B0/project/'
+
+const sendRequest = (path, callback) => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.timeout = 10000;
+
+  xhr.ontimeout = () => {
+    console.log('timeout!');
+  }
+
+  xhr.onreadystatechange = () => {
+ 
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(JSON.parse(xhr.responseText));
+      } else {
+        console.log('Error!', xhr.responseText);
+      }
+    }
+  }
+
+  xhr.open('GET', `${API}/${path}`);
+
+  
+
+  xhr.send();
+}
+
 class GoodsItems {
 constructor({title, price}) {
   this.title = title;
@@ -20,14 +49,23 @@ constructor(){
   this.goods = [];
   this.basket = basket;
 }
-fetchData(){
-  this.goods = [
-    { title: 'Ноутбук', price: 30000 },
-    { title: 'Клавиатура', price: 1000 },
-    { title: 'Мышь', price: 500 },
-    { title: 'Монитор', price: 10000 },
-  ];
+// fetchData(){
+//   this.goods = [
+//     { title: 'Ноутбук', price: 30000 },
+//     { title: 'Клавиатура', price: 1000 },
+//     { title: 'Мышь', price: 500 },
+//     { title: 'Монитор', price: 10000 },
+//   ];
+// }
+
+fetchData(callback) {
+  sendRequest('catalogData.json', (data) => {
+    this.goods = data;
+    callback();
+  });
 }
+
+
 
 addToBasket(item) {
   this.basket.add(item);
@@ -71,28 +109,24 @@ class Basket {
 
   }
 
-  changeQuantity() {
-
-  }
+ 
 
   clear() {
 
   }
 
-  fetchData() {
+//содержимое корзины
 
-  }
-
-  applyPromoCode() {
-
-  }
-
-  getDeliveryPrice() {
-
-  }
-
-  createOrder() {
-
+  fetchData(callback) {
+    fetch(`${API}/getBasket.json`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.goods = data;
+        callback();
+      });
   }
 
   getTotalPrice() {
@@ -127,9 +161,13 @@ class Basket {
 
 const basket = new Basket();
 const goodList = new GoodList(basket);
-goodList.fetchData();
+//goodList.fetchData();
 goodList.render();
-goodList.getTotalPrice();
+//goodList.getTotalPrice();
+goodList.fetchData(() => {
+  goodList.render();
+  goodList.getTotalPrice();
+});
 
 
 
