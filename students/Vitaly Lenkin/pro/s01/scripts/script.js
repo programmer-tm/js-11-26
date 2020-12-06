b79fe4d0167b3e46859b79ea5a65f334a8290d59
vl-gbr <wstat50@yandex.ru>
2020-12-06 20:05:39 +0300

    hw1 Доработка первого ДЗ

--- students/Vitaly Lenkin/pro/s01/scripts/script.js
+++ students/Vitaly Lenkin/pro/s01/scripts/script.js
@@ -1,6 +1,39 @@
 const goods = [{
   title: 'Ноутбук',
-  price: 30000
+  price: 30000,
+  image: "nout.webp"
+}, {
+  title: 'Клавиатура',
+  price: 1900,
+  image: "kbd01.jpg"
+}, {
+  title: 'Мышь',
+  price: 2500,
+  image: "mouse.jpeg"
+}, {
+  title: 'Монитор',
+  price: 30000,
+  image: "mon.jpg"
+}, {
+  title: 'Ноутбук',
+  price: 45000,
+  image: "nout02.png"
+}, {
+  title: 'Клавиатура',
+  price: 7000,
+  image: "kbd02.jpg"
+}, {
+  title: 'Мышь',
+  price: 700,
+  image: "mouse02.jpeg"
+}, {
+  title: 'Монитор',
+  price: 35000,
+  image: "mon02.jpg"
+}, {
+  title: 'Ноутбук',
+  price: 30000,
+  image: "nout.webp"
 }, {
   title: 'Клавиатура',
   price: 1000
@@ -9,20 +42,28 @@ const goods = [{
   price: 500
 }, {
   title: 'Монитор',
-  price: 10000
-}, ];
+  }, ];
 
-const eachGoodsItem = (title, price = 'Под заказ') =>
-  `
-  <div class="uk-card item">
-    <h5 class="uk-card-title">${title}</h5>
-    <p>${price}</p>
-    <button class="center uk-button uk-button-small">Купить</button>
+const eachGoodsItem = (title, price = 'Под заказ', image='') =>
+`
+  <!-- Product -->
+  <div class="card">
+      <a class="card-link" href="#">
+          <img src="images/${(image) ? image: 'nophoto.png'}" alt="photo product-1" class="card-img">
+      </a>
+      <div class="card-info">
+          <a href="" class="card-title">${title}</a>
+          <div class="card-descr"></div>
+          <div class="card-price">${price}
+              <img src="images/stars.png" alt="Raiting stars" class="card-stars">
+          </div>
+      </div>
+      <a href="#" class="add-to-cart">Add to Cart</a>
   </div>
 `;
 
 const renderGoods = (list = []) => {
-  const goodsList = list.map(item => eachGoodsItem(item.title, item.price));
+  const goodsList = list.map(item => eachGoodsItem(item.title, item.price, item.image));
   document.querySelector('.goods').innerHTML = goodsList.join('');
 };
 
