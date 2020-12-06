1306450d953ef339b3a2ec5c9a86fac11cad17e8
Andrew <movshenko11@yandex.ru>
2020-12-06 23:40:46 +0500

    lesson 2

--- students/Андрей Мовшенко/1 урок/script.js
+++ students/Андрей Мовшенко/1 урок/script.js
@@ -1,22 +1,104 @@
-const goods = [
-    { title: 'Ноутбук', price: 30000 },
-    { title: 'Клавиатура', price: 1000 },
-    { title: 'Монитор', price: 10000 },
-    { title: 'Мышь', price: 500 },
-  ];
-  
-  const renderGoodsItem = (title = "title", price = "price", link = "#") => {
-    return `<div class="goods-item">
-    <a class="item__link" href="${link}">
-    <img src="img/${title}.jpg" alt="#"></a>
-    <h3>${title}</h3>
-    <p>${price}</p>
-    <button>В корзину</button></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+  class GoodsItem {
+    constructor({ title, price }) {
+      this.title = title;
+      this.price = price;
+    }
+
+    render() {
+      return `
+      <div class="goods-item">
+      <a class="item__link" href="${this.link}">
+      <img src="img/${this.title}.jpg" alt="#"></a>
+      <h3>${this.title}</h3>
+      <p>${this.price}</p>
+      <button>В корзину</button></div>
+      `;
+    }
+  }
+
+  class GoodsList {
+    constructor() {
+      this.goods = [];
+    }
+
+    fetchData() {
+      this.goods = [
+        { title: 'Ноутбук', price: 30000 },
+        { title: 'Клавиатура', price: 1000 },
+        { title: 'Монитор', price: 10000 },
+        { title: 'Мышь', price: 500 },
+      ];
+    }
+
+    getTotalPrice () {
+      let total = 0
+      this.goods.forEach(item => {
+        total += item.price;
+      });
+      console.log(total);
+      return total;
+    }
+
+    render () {
+      const goodsList = this.goods.map(item => {
+        const goodsItem = new GoodsItem(item);
+        return goodsItem.render();
+      });
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    }
   }
     
-  renderGoodsList(goods);
+  const goodsList = new GoodsList();
+  goodsList.fetchData();
+  goodsList.render();
+
+  class Basket {
+    constructor() {
+      this.basketGoods = [];
+    }
+
+    addItem () {
+
+    }
+
+    removeItem () {
+
+    }
+
+    changeQuantity () {
+
+    }
+
+    clear () {
+
+    }
+
+    move () {
+
+    }
+
+    order () {
+
+    }
+
+    render ()
+  }
+
+  class BasketItem {
+    constructor ({title, price}) {
+      this.title = title;
+      this.price = price;
+    }
+
+
+    changeQuantity () {
+      
+    }
+
+    removeItem () {
+
+    }
+
+    render ()
+
+  }
