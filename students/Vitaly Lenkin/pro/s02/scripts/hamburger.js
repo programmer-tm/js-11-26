893eb332870d7624686f8545add0d1fa97bca01c
vl-gbr <wstat50@yandex.ru>
2020-12-13 17:06:21 +0300

    hw2 Гамбургеры всё.

--- students/Vitaly Lenkin/pro/s02/scripts/hamburger.js
+++ students/Vitaly Lenkin/pro/s02/scripts/hamburger.js
@@ -1,11 +1,10 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
 var sizeSel = document.getElementById('size');
 var stufSel = document.getElementById('stuffing');
 var toppSel = document.getElementById('topping');
 var createHamb = document.getElementById('create');
-var item = document.getElementById('iten');
+var item = document.querySelectorAll('.delete');
 
 var hamburgerMenu = {
    size: {
@@ -16,118 +15,117 @@ var hamburgerMenu = {
    stuffing: {
       cheese: {name: 'с сыром', price: 10, calories: 20},
       salad: {name: 'с салатом', price: 20, calories: 5},
-      potato: {name: 'с картошкой', price: 15, calories: 10}
-
+      potato: {name: 'с картошкой', price: 15, calories: 10}      
    },
    topping: {
       none: {name: 'по желанию', price: 0, calories: 0},
       standard: {name: 'с приправой', price: 15, calories: 0},
-      mayonez: {name: 'с майонезом', price: 20, calories: 5}
-
+      mayonez: {name: 'с майонезом', price: 20, calories: 5}      
    }
-
 }
 
 class CreateHamburger {
-   constructor () {
-      this.size = '';
-      this.stuffing = '';
-      this.topping = '';
+   constructor (size = '', stuffing = '', topping = '') {
+      this.size = size;
+      this.stuffing = stuffing;
+      this.topping = topping;
    }
+   //... Расчет полной стоимости гамбургера согласно опциям
    totalPrice(){
       return hamburgerMenu.size[this.size].price + hamburgerMenu.stuffing[this.stuffing].price + hamburgerMenu.topping[this.topping].price;
    }
+   //... Расчет калорийности гамбургера согласно опциям
    totalCalories(){
       return hamburgerMenu.size[this.size].calories + hamburgerMenu.stuffing[this.stuffing].calories + hamburgerMenu.topping[this.topping].calories;
 
    }
+   //... Инициализация формы ввода опций
    initForm() {
+      //... Размещаем опции размера гамбургера
       for ( var key in hamburgerMenu.size) {
-         // Create new option element
          var op = document.createElement('option');
          // Add class
-         op.className = 'uk-option sizeOpt';
+         op.className = 'uk-option uk-text-meta sizeOpt';
          op.value = key
-         // Add text node with option value         
          op.appendChild(document.createTextNode(hamburgerMenu.size[key].name));
          sizeSel.appendChild(op);
-
       };
+      //... Размещаем опции начинки гамбургера
       for (var key in hamburgerMenu.stuffing) {
-         // Create new option element
          var op = document.createElement('option');
-         // Add class
-         op.className = 'uk-option stufOpt';
-         // Add text node with input value
+         op.className = 'uk-option uk-text-meta stufOpt';
+         op.value = key
          op.appendChild(document.createTextNode(hamburgerMenu.stuffing[key].name));
          stufSel.appendChild(op);
-
       };
+      //... Размещаем опции заправки гамбургера
       for (var key in hamburgerMenu.topping) {
-         // Create new option element
          var op = document.createElement('option');
-         // Add class
-         op.className = 'uk-option toppOpt';
-         // Add text node with input value
+         op.className = 'uk-option uk-text-meta toppOpt';
+         op.value = key
          op.appendChild(document.createTextNode(hamburgerMenu.topping[key].name));
          toppSel.appendChild(op);
-
       }
    }
+   //... Создаем гамбургер по клику согласно опциям
    createOne (e) {      
       e.preventDefault();       
-      // Get input value
+      //... Читаем поля формы
       var selectedSize = sizeSel.value;
-      var selectedStaf = stafSel.value;
+      var selectedStaf = stufSel.value;
       var selectedTopp = toppSel.value;
                
-      // Create new li element
+      //... Создаем строку для гамбургера
       var li = document.createElement('li');
 
-      const gamb = new CreateHamburger (selectedSize, selectedStaf, (selectedTopp == 'по желанию') ? '' : selectedTopp);
-
-      // Add class
-      li.className = 'uk-margin list-item';
-      // Add text node with input value
-      li.appendChild(document.createTextNode(
-         `Гамбургер  ${gamb.size} ${gamb.stuffing} (${gamb.topping} -- Цена ${gamb.totalPrice()}, калорий ${gamb.totalCalories()}` ));
+      const gamb = new CreateHamburger (
+         selectedSize, 
+         selectedStaf, 
+         selectedTopp 
+         // (selectedTopp == 'по желанию') ? '' : selectedTopp // проверяем, но нне здесь
+      );
+      // li.className = 'uk-margin list-item';
+      li.style = 'width: 100%; display: flex; flex-direction: row; align-items:center;';
+      //... Всю сборку записываем в строку
+      //li.appendChild(document.createTextNode(`
+      //... Можно прямо в эмлемент без лишней переменной. пока так
+      var liHTML = 
+      `
+      <div style="width:30%">Гамбургер  <b>${hamburgerMenu.size[selectedSize].name.toUpperCase()}</b> </div>
+      <div style="width:20%"> ${hamburgerMenu.stuffing[selectedStaf].name} </div>
+      <div style="width:20%"> ${(gamb.topping == 'none') ? '' : hamburgerMenu.topping[selectedTopp].name}</div> 
+      <div style="width:26%"> --  ${gamb.totalPrice()} Р (<b>${gamb.totalCalories()}</b> калорий)</div>
+      `;
+      li.innerHTML = liHTML;
       
-      // Create del button element
-      var deleteBtn = document.createElement('button');
-      
-      // Add classes to del button
-      deleteBtn.className = '.uk-button .uk-button-danger uk-button-small uk-align-right delete';
-      
-      // Append text node
-      deleteBtn.appendChild(document.createTextNode('<span uk-icon="close"></span>'));
-      
-      // Append button to li
+      //... Создаем кнопку для удаления строки
+      var deleteBtn = document.createElement('button');      
+      deleteBtn.className = 'uk-button uk-button-danger uk-button-small uk-align-center-right uk-flex delete';      
+      deleteBtn.appendChild(document.createTextNode('X'));
+      // deleteBtn.appendChild(document.createTextNode('<span uk-icon="close"></span>'));
       li.appendChild(deleteBtn);
-      
-      // Append li to list
-      item.appendChild(li);
+      deleteBtn.addEventListener('click', gamb.removeItem)
+      itemList.appendChild(li);
    }
+   //... Удаление строки по клику
    removeItem(e){
       if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
+        if(confirm('Точно удалять?')){
           var li = e.target.parentElement;
           itemList.removeChild(li);
         }
       }
     }
 }
-
-
+//... Инициализируем пустой гамбургер, что получить доступ к форме
 const gamb = new CreateHamburger;
 gamb.initForm();
-gamb.createOne();
+//... Вешаем обработчик на кнопку "создать"
+createHamb.addEventListener('click', gamb.createOne);
+// gamb.createOne();    // трейсер
+// console.log( gamb )  // трейсер
+//!item.forEach(addEventListener('click', gamb.removeItem)); // убрас в элементы
 
-console.log ( gamb, gamb.totalPrice(), gamb.totalCalories() );
+// console.log ( gamb, gamb.totalPrice(), gamb.totalCalories() );
 
-// Form submit event
-form.addEventListener('submit', gamb.createOne);
-item.addEventListener('click', gamb.removeItem);
-// Delete event
-
-// Remove item
 
