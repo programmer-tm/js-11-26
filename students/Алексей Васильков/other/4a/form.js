212470e31caa15c84c8c39e9482a5f6227065494
Dragon <dragon.program.sib@gmail.com>
2020-12-15 05:28:12 +0700

    Update task 3 (lesson 4).

--- students/Алексей Васильков/other/4a/form.js
+++ students/Алексей Васильков/other/4a/form.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // 3. *Создать форму обратной связи с полями: Имя, Телефон, E-mail, текст, кнопка Отправить. При нажатии на кнопку Отправить произвести валидацию полей следующим образом:
 // a. Имя содержит только буквы.
 // b. Телефон имеет вид +7(000)000-0000.
@@ -5,28 +7,26 @@
 // d. Текст произвольный.
 // e. Если одно из полей не прошло валидацию, необходимо выделить это поле красной рамкой и сообщить пользователю об ошибке.
 
-'use strict';
-
-const check = () => {
+let check = () => {
     const
-        nameCheck = /^[aA-zZ] +$/g,
-        phoneCheck = /^\+7\(\d{3})\d{3}-\d{4}$/g,
-        emailCheck = /^([aA-zZ]{6}+)@([aA-zZ]{4}+)\.([a-z]{2})$/g || /^([aA-zZ]{2}+)\.([aA-zZ]{4}+)@([aA-zZ]{4}+)\.([a-z]{2})$/g || /^([aA-zZ]{2}+)\-([aA-zZ]{4}+)@([aA-zZ]{4}+)\.([a-z]{2})$/g,
-        inputName = document.form.name.value,
-        inputPhone = document.form.phone.value,
-        inputEmail = document.form.email.value;
+        inputName = document.forms.form.form_name.value,
+        inputPhone = document.forms.form.form_phone.value,
+        inputEmail = document.forms.form.form_email.value,
+        nameCheck = /^[aA-zZ-аА-яЯ]+$/g,
+        phoneCheck = /^[+7]+[(]+\d{3}[)]+\d{3}\-\d{4}$/g,
+        emailCheck = /^[a-z]{6}+[a-z]{4}+\.[a-z]{2}$/g || /^([a-z]{2})\.([a-z]{4})\@([a-z]{4})\.([a-z]{2})$/g || /^([a-z]{2})\-([a-z]{4})\@([a-z]{4})\.([a-z]{2})$/g;
 
-    if(!inputName.match(nameCheck)) {
+    if (!inputName.match(nameCheck)) {
         alert('Имя, должно состоять только из букв!');
         return false;
-    } else if(!inputPhone.match(phoneCheck)) {
+    } else if (!inputPhone.match(phoneCheck)) {
         alert('Телефон, должен быть в формате: +7(000)000-0000!');
         return false;
-    } else if(!inputEmail.match(emailCheck)) {
+    } else if (!inputEmail.match(emailCheck)) {
         alert('Введите, корректную форму email!');
         return false;
-    } else{
+    } else {
         alert('Ваше сообщение, отправлено!');
     }
     return false;
-};
+}
