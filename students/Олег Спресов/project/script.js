aead01d24097ba96638c62ee5e764e195afd2f5f
OlegSP <spoliv@rambler.ru>
2020-12-14 22:46:20 +0300

    fourth commit

--- students/Олег Спресов/project/script.js
+++ students/Олег Спресов/project/script.js
@@ -1,4 +1,4 @@
-const API = 'https://raw.githubusercontent.com/spoliv/js-11-26/master/students/Олег%20Спресов/catalogData.json';
+const API = 'https://raw.githubusercontent.com/spoliv/js-11-26/master/students/Олег%20Спресов/responses';
 
 
 const sendRequest = (path) => {
@@ -23,7 +23,7 @@ const sendRequest = (path) => {
       }
     }
 
-    xhr.open('GET', `${API}`);
+    xhr.open('GET', `${API}/${path}`);
 
     // xhr.setRequestHeader('Content-Type', 'application/json');
 
@@ -43,30 +43,11 @@ class GoodsItem {
         <img src=${this.image} alt="product">
         <h4 class="prod_name">${this.title}</h4>
         <p class="prod_price">${this.price} <span>руб.</span></p>
-        <button type="button" class="add_cart">Добавить в <i class="fa fa-shopping-cart"></i></button>
+        <button type="button" class="add_cart" name="add-to-basket">Добавить в <i class="fa fa-shopping-cart"></i></button>
       </div>`;
   }
 }
 
-
-
-
-//   constructor({ id_product, product_name, price }) {
-//     this.id = id_product;
-//     this.title = product_name;
-//     this.price = price;
-//   }
-//
-//   render() {
-//     return `
-//       <div class="item" data-id="${this.id}">
-//         <h4>${this.title}</h4>
-//         <p>${this.price}</p>
-//         <button type="button" name="add-to-basket">Add to basket</button>
-//       </div>
-//     `;
-//   }
-// }
 
 class GoodsList {
   constructor(basket) {
@@ -77,6 +58,7 @@ class GoodsList {
       if (event.target.name === 'add-to-basket') {
         const id = event.target.parentElement.dataset.id;
         const item = this.goods.find((goodsItem) => goodsItem.id_product === parseInt(id));
+        console.log(item)
         if (item) {
           this.addToBasket(item);
         } else {
@@ -88,7 +70,7 @@ class GoodsList {
 
   fetchData() {
     return new Promise((resolve, reject) => {
-      sendRequest(`${API}`)
+      sendRequest('catalogData.json')
         .then((data) => {
           this.goods = data;
           resolve();
@@ -216,120 +198,3 @@ goodsList.fetchData()
     goodsList.getTotalPrice();
   });
 
-
-
-
-
-
-
-
-
-
-
-// class GoodsItem {
-//   constructor(title, price, image) {
-//     this.title = title;
-//     this.price = price;
-//     this.image = image;
-//   }
-//   render() {
-//     return `<div class="item">
-//         <img src=${this.image} alt="product">
-//         <h4 class="prod_name">${this.title}</h4>
-//         <p class="prod_price">${this.price} <span>руб.</span></p>
-//         <button type="button" class="add_cart">Добавить в <i class="fa fa-shopping-cart"></i></button>
-//       </div>`;
-//   }
-// }
-//
-//
-// class GoodsList {
-//     constructor() {
-//         this.goods = [];
-//     }
-//
-//     fetchGoods() {
-//         this.goods = [
-//             {title: 'Лампа', price: 3000, image: 'img/lamp.jpg'},
-//             {title: 'Кресло', price: 13000, image: 'img/arm-chair.jpg'},
-//             {title: 'Диван', price: 25000, image: 'img/divan.jpg'},
-//             {title: 'Торшер', price: 9500, image: 'img/torsher.jpg'},
-//             {title: 'Ваза', price: 2000, image: 'img/vase.jpg'},
-//             {title: 'Часы', price: 2700, image: 'img/clock.jpg'},
-//             {title: 'Ступка керамическая', price: 4300, image: 'img/stupka.jpg'},
-//             {title: 'Чайник', price: 1350, image: 'img/kettle.jpg'},
-//             {title: 'Стул', price: 2700, image: 'img/chair.jpg'},
-//             {title: 'Подвесная лампа', price: 7230, image: 'img/suspended_lamp.jpg'},
-//         ];
-//     }
-//
-//     render() {
-//         const goodsList = this.goods.map(item => {
-//             const goodItem = new GoodsItem(item.title, item.price, item.image);
-//             return goodItem.render();
-//         });
-//         document.querySelector('.goods').innerHTML = goodsList.join('');
-//     }
-//
-//     calculateSumPrices() {
-//         let sum_prices = 0;
-//         this.goods.forEach(good => {
-//             const goodItem = new GoodsItem(good.title, good.price, good.image);
-//             sum_prices += goodItem.price;
-//         })
-//         //return sum_prices
-//         console.log(sum_prices)
-//     }
-// }
-//
-// class Basket {
-//     constructor() {
-//         this.basketGoods = [];
-//     }
-//     render() {
-//
-//
-//     }
-//     addToBasket() {
-//
-//
-//     }
-//
-//     removeFromBasket() {
-//
-//     }
-//
-//     editBasket() {
-//
-//
-//     }
-//     calculateBasketCost() {
-//
-//
-//     }
-// }
-//
-// class BasketItem {
-//     constructor(title, price, quantity=1) {
-//         this.title = title;
-//         this.price = price;
-//         this.quantity = quantity;
-//     }
-//     render() {
-//
-//     }
-//     editQuantity() {
-//
-//
-//     }
-//     calculateItemCost() {
-//
-//     }
-// }
-//
-//
-//
-// // const list = new GoodsList();
-// // list.fetchGoods();
-// // list.render();
-// // list.calculateSumPrices();
