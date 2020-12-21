27af583d965331d620de2a89c5807159ac0165b8
Andrew <movshenko11@yandex.ru>
2020-12-22 01:00:11 +0500

    lesson 6

--- students/Андрей Мовшенко/1 урок/script.js
+++ students/Андрей Мовшенко/1 урок/script.js
@@ -27,6 +27,87 @@
     });
   }
 
+  Vue.component('v-error', {
+    props: ['message'],
+    template: `
+    <div>
+      Ошибка! {{ message }}
+    </div> 
+    `,
+  });
+
+  Vue.component('v-search', {
+    props: ['value'],
+    template: `
+    <input type="text" :value="value" @input="$emit('input', $event)" class="search" v-model="value" placeholder="Search..."/>
+    `,
+  });
+
+  Vue.component('v-basket', {
+    props: ['goods'],
+    template: `
+    <div class="cart">
+      <div class="cart-item" v-for="Item in goods">
+        <p class="cart-item__title">{{item.product_name}}</p>
+        <p>{{item.quantity}} x {{item.price}}</p>
+      </div>
+    </div>
+    `,
+  });
+
+  Vue.component('v-header', {
+    template: `
+     <header class="center">
+       <span class="logo">E-Shop</span>
+       <slot />
+       <button @click="handleClick" class="cart-button" type="button">Корзина</button>
+       <slot name="basket" />
+     </header>
+    `,
+    methods: {
+      handleClick () {
+        this.$emit('change-is-cart-visible');
+      }
+    }
+  });
+
+  Vue.component('v-goods', {
+    props: ['goods'],
+    template: `
+    <main>
+      <div class="goods center">
+        <v-item 
+          v-for="item in goods" 
+          v-bind:element="item"
+          v-on:addToBasket="handleAddToBasket"
+          />
+        <div v-if="!goods.lenght" class="goods-empty">Нет данных</div>
+      </div>
+    </main>
+    `,
+    methods: {
+      handleAddToBasket(data) {
+        this.$emit('add', data); 
+      },
+    }
+  });
+
+  Vue.component('v-item', {
+    props: ['element'],
+    template: `
+      <div class="item">
+        <h3>{{element.product_name}}</h3>
+        <p>{{element.price}}</p>
+        <button type="button" v-on:click="addToBasket">В корзину</button>
+      </div>
+    `,
+    methods: {
+      addToBasket() {
+        this.$emit('addToBasket', this.element);
+      }
+    }
+  });
+
   new Vue({
     el: '#app',
     data: {
@@ -34,6 +115,7 @@
       basketGoods: [],
       searchValue: '',
       isVisible: false,
+      errorMessage: '',
     },
     mounted() {
       this.fetchData();
@@ -46,6 +128,9 @@
             .then((data) => {
               this.goods = data;
               resolve();
+            })
+            .catch ((error) => {
+              this.errorMessage = 'Не удалось получить список товаров'
             });
         });
       },
@@ -55,23 +140,28 @@
             .then((data) => {
               this.goods = data;
               resolve();
+            })
+            .catch ((error) => {
+              this.errorMessage = 'Не удалось получить список товаров'
             });
         });
       },
-      addToBasket(){
+      addToBasket(item){
         const index = this.basketGoods.findIndex((basketItem) => basketItem.id_product === item.id_product);
         if (index > -1) {
           this.basketGoods[index].quantity += 1;
         } else {
-          this.basketGoods.push (item);
+          this.basketGoods.push(item);
         }
         console.log(this.basketGoods);
       },
       removeItem (id){
         this.basketGoods = this.basketGoods.filter((goodsItem) => goodsItem.id_product != parseInt(id));
+        console.log(this.basketGoods);
       },
-      handleCartButtonClick(){
-        this.isVisible = !this.isVisible;
+      handleSearchInput(event) {
+        console.log (event);
+        this.searchValue = event.target.value; 
       }
     },
     computed: {
