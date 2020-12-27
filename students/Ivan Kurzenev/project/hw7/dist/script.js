ff8c4d84ac35cdb899b030052007e803a386d99b
variasco <vakin213@gmail.com>
2020-12-27 18:37:10 +0300

    hw7

---
+++ students/Ivan Kurzenev/project/hw7/dist/script.js
@@ -0,0 +1,285 @@
+const API = 'http://localhost:3000/api/';
+
+const sendRequest = (path, method = 'GET', body = {}) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.timeout = 10000;
+
+    xhr.ontimeout = () => {
+      console.log('timeout!');
+    }
+
+    xhr.onreadystatechange = () => {
+      // console.log('ready state change', xhr.readyState);
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          console.log('Error!', xhr.responseText);
+          reject(xhr.responseText);
+        }
+      }
+    }
+
+    xhr.open(method, `${API}${path}`);
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.send(JSON.stringify(body));
+  });
+}
+
+Vue.component('v-header', {
+  props: [
+    'search',
+  ],
+  template: `
+    <header class="header padding">
+      <a href="index.html"><span class="logo">E-Shop</span></a>
+      <slot />
+      <button @click="handleIsCartVisible" type="button" class="cart-button">Корзина</button>
+    </header>
+  `,
+  methods: {
+    handleIsCartVisible(data) {
+      this.$emit('display', data);
+    },
+  },
+});
+
+Vue.component('v-goods', {
+  props: [
+    'goods',
+    'cartGoods',
+    'isCartVisible',
+    'totalPrice',
+  ],
+  template: `
+    <main class="main container">
+        <section class="goods">
+            <v-item 
+              v-for="item in goods" 
+              :element="item"
+              @addToCart="handleAddToCart"
+            />
+            <p v-if="goods.length <=0 " class="emptyGoods">
+                Нет данных
+            </p>
+            <v-cart 
+              v-if="isCartVisible"
+              :cart-goods="cartGoods"
+              :total-price="totalPrice"
+              @deleteElement="handleDelete"
+              @clear="handleClearCart"
+            />
+        </section>
+    </main>
+  `,
+  methods: {
+    handleAddToCart(data) {
+      this.$emit('add', data);
+    },
+    handleDelete(data) {
+      this.$emit('remove', data);
+    },
+    handleClearCart() {
+      this.$emit('clear');
+    }
+  },
+});
+
+Vue.component('v-item', {
+  props: [
+    'element',
+  ],
+  template: `
+    <div class="item">
+        <a class="item__link" href="#">
+            <img class="item__image" :src="element.path" :alt="element.title">
+        </a>
+        <h4 class="item__title marginTop">{{element.title}}</h4>
+        <p class="item__price marginTop">Цена: {{element.price}} &#8381;</p>
+        <button @click="addToCart" type="button" class="item__buy-button marginTop">В
+            корзину</button>
+    </div>
+  `,
+  methods: {
+    addToCart() {
+      this.$emit('addToCart', this.element)
+    },
+  },
+});
+
+Vue.component('v-cart', {
+  props: [
+    'cartGoods',
+    'totalPrice',
+  ],
+  template: `
+    <section class="cart">
+        <v-cart-item 
+          v-for="item in cartGoods"
+          :element="item"
+          @deleteElement="remove"
+        />
+        <p v-if="cartGoods.length <= 0" class="emptyGoods">
+            Корзина пуста
+        </p>
+        <p class=" item__price marginTop">Общая цена: {{this.totalPrice}} &#8381;</p>
+        <button @click="clearCart" type="button" class="item__buy-button marginTop">Очистить
+            корзину</button>
+    </section>
+  `,
+  methods: {
+    remove(data) {
+      this.$emit('deleteElement', data);
+    },
+    clearCart() {
+      this.$emit('clear')
+    }
+  },
+})
+
+Vue.component('v-cart-item', {
+  props: [
+    'element',
+  ],
+  template: `
+    <div class="cartItem">
+        <div class="cartItem__left">
+            <h4 class="item__title">{{element.title}} x{{element.quantity}}</h4>
+            <p class="item__price marginTop">Цена: {{element.price}} &#8381;</p>
+        </div>
+        <div class="cartItem__right">
+            <button @click="remove" type="button"
+                class="item__buy-button">Удалить</button>
+        </div>
+    </div>
+  `,
+  methods: {
+    remove() {
+      this.$emit('deleteElement', this.element);
+    },
+  },
+});
+
+Vue.component('v-search', {
+  props: ['value'],
+  template: `
+    <input :value="value" @input="$emit('input', $event.target.value)" type="search" class="search" placeholder="Search...">
+  `
+});
+
+Vue.component('v-error', {
+  props: ['message'],
+  template: `
+    <p class="error">
+      {{this.message}}
+    </p>
+  `
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    cartGoods: [],
+    searchValue: '',
+    isVisibleCart: false,
+    errorMessage: '',
+  },
+  mounted() {
+    this.fetchData();
+    this.fetchDataCart();
+  },
+  methods: {
+    fetchData() {
+      return new Promise((resolve, reject) => {
+        sendRequest('data')
+          .then((data) => {
+            this.goods = data;
+            resolve();
+          })
+          .catch(() => {
+            this.errorMessage = `Не удалось загрузить данные списка товаров!`;
+          });
+      })
+    },
+    fetchDataCart() {
+      return new Promise((resolve, reject) => {
+        sendRequest('dataCart')
+          .then((data) => {
+            this.cartGoods = data;
+            resolve();
+          })
+          .catch(() => {
+            alert(`Не удалось загрузить данные корзины!`)
+          });
+      })
+    },
+    addToCart(item) {
+      sendRequest('dataCart', 'POST', item)
+        .then((result) => {
+          console.log('Result', result);
+          if (!result.success) {
+            console.log('addToBasket Error');
+            return;
+          }
+
+          const index = this.cartGoods.findIndex((element) => element.id_product === item.id_product);
+          if (index > -1) {
+            this.cartGoods[index].quantity += 1;
+          } else {
+            item.quantity = 1;
+            this.cartGoods.push(item);
+          }
+        })
+        .catch((error) => {
+          this.errorMessage = 'Не удалось добавить элемент в корзину!';
+        });
+    },
+    deleteFromCart(item) {
+      sendRequest('dataCart', 'POST', item)
+        .then((result) => {
+          console.log('Result', result);
+          if (!result.success) {
+            console.log('deleteFromCart Error');
+            return;
+          }
+
+          this.cartGoods = this.cartGoods.filter((element) => element.id_product !== item.id_product);
+        })
+        .catch((error) => {
+          this.errorMessage = 'Не удалось удалить элемент корзины!';
+        });
+    },
+    clearCart() {
+      sendRequest('dataCart', 'POST')
+        .then((result) => {
+          console.log('Result', result);
+          if (!result.success) {
+            console.log('deleteFromCart Error');
+            return;
+          }
+
+          this.cartGoods = [];
+        })
+        .catch((error) => {
+          this.errorMessage = 'Не удалось удалить элемент корзины!';
+        });
+    },
+    isVisibleCartToggler() {
+      this.isVisibleCart = !this.isVisibleCart;
+    },
+  },
+  computed: {
+    filteredGoods() {
+      const regexp = new RegExp(this.searchValue.trim(), 'i');
+      return this.goods.filter((goodsItem) => regexp.test(goodsItem.title));
+    },
+    totalPrice() {
+      return this.cartGoods.reduce((acc, curVal) => acc + curVal.price * curVal.quantity, 0);
+    },
+  },
+});
---
+++ students/Ivan Kurzenev/project/hw7/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.static('./dist'));
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+    fs.readFile('./data.json', 'utf-8', (err, rawData) => {
+        if (err) {
+            console.log('Read data.json error!', err);
+            res.status(500).send('Server error');
+            return;
+        }
+        res.send(rawData);
+    });
+});
+
+app.get('/api/dataCart', (req, res) => {
+    fs.readFile('./dataCart.json', 'utf-8', (err, rawData) => {
+        if (err) {
+            console.log('Read dataCart.json error!', err);
+            res.status(500).send('Server error');
+            return;
+        }
+        res.send(rawData);
+    });
+});
+
+app.post('/api/dataCart', (req, res) => {
+    fs.readFile('./dataCart.json', 'utf-8', (err, rawData) => {
+        if (err) {
+            console.log('Read dataCart.json error!', err);
+            res.status(500).send('Server error');
+            return;
+        }
+
+        const basket = JSON.parse(rawData);
+
+        const item = req.body;
+
+        const index = basket.findIndex((basketItem) => basketItem.id_product === item.id_product);
+        if (index > -1) {
+            basket[index].quantity += 1;
+        } else {
+            basket.push({
+                ...item,
+                quantity: 1
+            });
+        }
+
+        fs.writeFile('./dataCart.json', JSON.stringify(basket), (err) => {
+            if (err) {
+                console.log('Write dataCart.json error!', err);
+                res.status(500).send('Server error');
+                return;
+            }
+
+            res.json({
+                success: true
+            });
+        });
+
+    });
+});
+
+app.delete('/api/dataCart/:id', (req, res) => {
+    fs.readFile('./dataCart.json', 'utf-8', (err, rawData) => {
+        if (err) {
+            console.log('Read dataCart.json error!', err);
+            res.status(500).send('Server error');
+            return;
+        }
+
+        let basket = JSON.parse(rawData);
+
+        const id = parseInt(req.params.id);
+
+        basket = basket.filter((goodsItem) => goodsItem.id_product !== id);
+
+        fs.writeFile('./dataCart.json', JSON.stringify(basket), (err) => {
+            if (err) {
+                console.log('Write dataCart.json error!', err);
+                res.status(500).send('Server error');
+                return;
+            }
+
+            res.json({
+                success: true
+            });
+        });
+
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
